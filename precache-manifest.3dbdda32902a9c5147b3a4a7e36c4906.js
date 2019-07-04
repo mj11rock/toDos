c5138e3907a1e9028e6883029e47c08a1f5cbfc6
mj11rock <mj11rock@gmail.com>
2019-07-04 23:20:49 +0500

    Updates

---
+++ precache-manifest.3dbdda32902a9c5147b3a4a7e36c4906.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db11fff01b14274d3c1c6eef426c5e4f",
+    "url": "/toDos/index.html"
+  },
+  {
+    "revision": "df01803159e89c2b3f1e",
+    "url": "/toDos/static/css/main.729eb98f.chunk.css"
+  },
+  {
+    "revision": "146906bbc55ccde847cd",
+    "url": "/toDos/static/js/2.98d18a7b.chunk.js"
+  },
+  {
+    "revision": "df01803159e89c2b3f1e",
+    "url": "/toDos/static/js/main.340245c9.chunk.js"
+  },
+  {
+    "revision": "e16c57cf70fd2331bee7",
+    "url": "/toDos/static/js/runtime~main.00dd7ab5.js"
+  }
+]);
